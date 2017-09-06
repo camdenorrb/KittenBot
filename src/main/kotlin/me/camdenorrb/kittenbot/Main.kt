9298dc413bae6e72170b5235a231123b6f0e65d4
camdenorrb <camdenorrb@users.noreply.github.com>
2017-09-06 04:23:43 -0500

    Main init

--- src/main/kotlin/me/camdenorrb/kittenbot/Main.kt
+++ src/main/kotlin/me/camdenorrb/kittenbot/Main.kt
@@ -1,3 +1,10 @@
 package me.camdenorrb.kittenbot
 
 // TODO: Allow sharding by having multiple instances of KittenBots, mapped to their ShardID
+
+object Main {
+
+	@JvmStatic
+	fun main(args: Array<String>) = Unit
+
+}
